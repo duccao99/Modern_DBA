7e483d8c170bc3e96e57ae95d9991b5c9d533085
Duc Cao <caovanducs@gmail.com>
2021-05-12 15:31:48 +0700

    Onboarding redis part 2

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const responseTime = require("response-time");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(responseTime());
 
 app.get("/", function (req, res) {
   res.json({ message: "hi" });
--- routes/redis.route.js
+++ routes/redis.route.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const redis = require("redis");
 const client = redis.createClient();
+const axios = require("axios");
 
 const base_data = [
   {
@@ -33,6 +34,44 @@ router.get("/", function (req, res) {
 
     redis_host: client.host,
     redis_port: client.port,
+  });
+});
+
+router.get("/search", function (req, res) {
+  // Extract the query from url and trim trailing spaces
+  console.log(req.query);
+  const query = req.query.query.trim();
+  // Build the Wikipedia API url
+  const searchUrl = `https://en.wikipedia.org/w/api.php?action=parse&format=json&section=0&page=${query}`;
+
+  // Try fetching the result from Redis first in case we have it cached
+  return client.get(`wikipedia:${query}`, (err, result) => {
+    // If that key exist in Redis store
+    if (result) {
+      const resultJSON = JSON.parse(result);
+      return res.status(200).json(resultJSON);
+    } else {
+      // Key does not exist in Redis store
+      // Fetch directly from Wikipedia API
+      return axios
+        .get(searchUrl)
+        .then((response) => {
+          const responseJSON = response.data;
+          // Save the Wikipedia API response in Redis store
+          client.setex(
+            `wikipedia:${query}`,
+            3600,
+            JSON.stringify({ source: "Redis Cache", ...responseJSON })
+          );
+          // Send JSON response to client
+          return res
+            .status(200)
+            .json({ source: "Wikipedia API", ...responseJSON });
+        })
+        .catch((err) => {
+          return res.json(err);
+        });
+    }
   });
 });
 
