6b78b1840362fad6ed4d428e92c2da21c9dd6640
Duc Cao <caovanducs@gmail.com>
2021-06-01 15:21:51 +0700

    Feature Add To Cart By Redis

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const redis = require('redis');
 const db = redis.createClient();
+const proModel = require('../models/product.model');
+const handlebars = require('handlebars');
 
 /*
  * Command
@@ -8,14 +10,64 @@ const db = redis.createClient();
     2. smembers key - get all data
     3. FLUSHDB - clear db
 */
+router.get('/', async function (req, res) {
+  db.smembers('cart_id:3:pro_id', async (er, ret) => {
+    if (er) {
+      return res.status(500).json({
+        message: er
+      });
+    } else {
+      const cart = ret;
+      console.log(ret);
+      let cart_data = [];
+
+      for (let i = 0; i < cart.length; ++i) {
+        const pro = await proModel.find({
+          _id: cart[i]
+        });
+
+        const templateProName = handlebars.compile('{{this.proName}}');
+        const retProName = templateProName({ proName: pro[0].proName });
+
+        const templatePrice = handlebars.compile('{{this.price}}');
+        const retPrice = templatePrice({ price: pro[0].price });
+
+        const templateCategory = handlebars.compile('{{this.category}}');
+        const retCategory = templateCategory({ category: pro[0].category });
+
+        const templateAvatarUrl = handlebars.compile('{{this.avatarUrl}}');
+        const retAvatarUrl = templateAvatarUrl({
+          avatarUrl: pro[0].avatarUrl
+        });
+
+        const templateProId = handlebars.compile('{{this.proId}}');
+        const retProId = templateProId({
+          proId: pro[0]._id
+        });
+
+        cart_data.push({
+          proId: retProId,
+          proName: retProName,
+          price: retPrice,
+          category: retCategory,
+          avatarUrl: retAvatarUrl
+        });
+      }
+      console.log(cart_data);
+
+      return res.render('vwCart/vwCart', {
+        layout: 'layout',
+        cart: cart_data
+      });
+    }
+  });
+});
 
 router.get('/add-to-cart', async function (req, res) {
   res.render('vwCart/vwAddToCart', {
     layout: 'layout'
   });
 });
-
-router.get('/', async function (req, res) {});
 
 router.post('/', async function (req, res) {
   if (!req.body.user_id || !req.body.pro_id) {
