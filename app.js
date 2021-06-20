471f4e854c24cece5276fff92ea5752db66b1d4a
kirkorz <kkikko512@gmail.com>
2021-06-20 15:03:07 +0700

    redis recent view

--- app.js
+++ app.js
@@ -1,12 +1,110 @@
 const express = require("express");
+const app = express();
 const cors = require("cors");
 const path = require('path');
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+const crypto = require('crypto');
 var bodyParser = require('body-parser');
+// session redis
+const Dbquery = require("./mongodb_query/dataquery");
+const session = require('express-session');
+app.use(session({
+  secret:"bao2287",
+  cookie: {
+    secure: false, // if true only transmit cookie over https
+    httpOnly: false, // if true prevent client side JS from reading the cookie 
+    maxAge: 1000 * 60 * 10 // session max age in miliseconds
+  }
+}))
+// const RedisStore = require('connect-redis')(session);
+// const redis = require('redis');
+// // const redisClient  = redis.createClient()
+// const redisClient  = redis.createClient({
+//   host: 'redis-19009.c253.us-central1-1.gce.cloud.redislabs.com',
+//   port: 19009,
+//   password: '0EUqKArE2aWKf6sO0UihOtIJaNAx96Qi'
+// });
+// app.use(session({
+//   store: new RedisStore({ client: redisClient }),
+//   secret: 'bao2287',
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: {
+//       secure: false, // if true only transmit cookie over https
+//       httpOnly: false, // if true prevent client side JS from reading the cookie 
+//       maxAge: 1000 * 60 * 10 // session max age in miliseconds
+//   }
+// }))
+// app.get("/getses/:productId", async (req, res) => {
+//   try {
+//     // console.log(req.cookies)
+//     const isbn = req.params.productId;
+//     // const sess = req.session;
+//     var cok = req.cookies.accessId
+//     // console.log(cok)
+//     if(cok){
+//       console.log(cok)
+//       // res.status(200).send({"ok":1});
+//     } else {
+//       // console.log("no");
+//       var cok = crypto.randomBytes(48).toString('hex')
+//       res.cookie('accessId', cok)
+//         // req.session.accessId = token
+//         // res.end("success")
+//     }
+//     // console.log(cok)
+//     // console.log(req.session.accessId)
+//     redisClient.lrange(`${cok}`,0,-1, async (err, list_result) => {
+//         if (err) throw err;
+//         if (list_result) {
+//           const temp = JSON.parse(JSON.stringify(await Dbquery.getDetail(isbn)));
+//           var result = []
+//           list_result.forEach(function (item, index) {
+//             result.push(JSON.parse(item));
+//           });
+//           var listid = []
+//           result.forEach(function(item){
+//             listid.push(item['_id'])
+//           })
+//           if(!listid.includes(temp['_id'])){
+//             redisClient.rpush(`${cok}`,JSON.stringify(temp));
+//           }
+//           // console.log(result)
+//           res.status(200).json(result);
+//           // res.status(200).send({
+//           //     jobs: JSON.parse(result),
+//           //     message: "data retrieved from the cache"
+//           // });
+//         } else {
+//           console("part1")
+//           const result = await Dbquery.getDetail(isbn);
+//           // redisClient.setex(`${cok}`, 600, JSON.stringify(result));
+//           redisClient.rpush(`${cok}`,JSON.stringify(result));
+//           res.status(200).json(result);
+//           // res.status(200).send({
+//           //     jobs: result,
+//           //     message: "cache miss"
+//           // });
+//         }
+//     });
+//   } catch(err) {
+//       res.status(500).send({message: err.message});
+//   }
+// });
+// app.get("/setses", (req, res) => {
+//   require('crypto').randomBytes(48, function(err, buffer) {
+//     var token = buffer.toString('hex');
+//     req.session.accessId = token
+//     res.end("success")
+//   });
+// });
 
+
+
+// 
 const multer  = require('multer')
 const upload = multer();
-
-const app = express();
 
 const responseTime = require("response-time");
 app.use(responseTime());
--- routes/views.routes.js
+++ routes/views.routes.js
@@ -3,6 +3,8 @@ var router = express.Router();
 const Dbquery = require("../mongodb_query/dataquery");
 const Neo4jquery = require("../mongodb_query/neo4jquery");
 const AuthMiddleWare = require("../middleware/auth");
+const crypto = require('crypto');
+
 
 const redis = require("redis");
 const client = redis.createClient({
@@ -16,7 +18,54 @@ router.get("/detail/:productId",async(req,res)=>{
     try {
         const result = await Dbquery.getDetail(req.params.productId);
         const pwith = await Neo4jquery.getWith(req.params.productId);
-        return res.render('detail',{result,pwith});
+        var cok = req.cookies.accessId
+        if(cok){
+            res.cookie('accessId', cok, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true })
+        // res.status(200).send({"ok":1});
+        } else {
+        // console.log("no");
+            var cok = crypto.randomBytes(48).toString('hex')
+            res.cookie('accessId', cok, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true })
+            // req.session.accessId = token
+            // res.end("success")
+        }
+        client.lrange(`${cok}`,0,-1, async (err, list_result) => {
+            if (err) throw err;
+            if (list_result) {
+              const temp = JSON.parse(JSON.stringify(await Dbquery.getDetail(req.params.productId)));
+              var result_h = []
+              list_result.forEach(function (item, index) {
+                result_h.push(JSON.parse(item));
+              });
+              var listid = []
+              result_h.forEach(function(item){
+                listid.push(item['_id'])
+              })
+              if(!listid.includes(temp['_id'])){
+                client.rpush(`${cok}`,JSON.stringify(temp));
+              }
+              client.ltrim(`${cok}`,-4,-1);
+              client.expire(`${cok}`, 60 * 60 * 24)
+              return res.render('detail',{result,pwith,result_h});
+              // console.log(result)
+              //res.status(200).json(result);
+              // res.status(200).send({
+              //     jobs: JSON.parse(result),
+              //     message: "data retrieved from the cache"
+              // });
+            } else {
+              const product = await Dbquery.getDetail(req.params.productId);
+              // redisClient.setex(`${cok}`, 600, JSON.stringify(result));
+              client.rpush(`${cok}`,JSON.stringify(product));
+              var result_h = []
+              return res.render('detail',{result,pwith,result_h});
+              //res.status(200).json(result);
+              // res.status(200).send({
+              //     jobs: result,
+              //     message: "cache miss"
+              // });
+            }
+        });
     } catch (error) {
         console.log(error);
         return res.status(500).json(error);
